e0039ab08b5dcbf4892dce8327b3e1ea2a107bdb
Maksym Lahutin <103822447+Minutestatic@users.noreply.github.com>
2023-09-17 13:33:13 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -61,7 +61,7 @@ const App = () => {
 
       <main>
         <Suspense fallback={<Loader />}>
-          <Routes base="/goit-react-hw-08-phonebook/">
+          <Routes basename="/goit-react-hw-08-phonebook/">
             {appRoutes.map(({ path, element }) => (
               <Route key={path} path={path} element={element} />
             ))}
